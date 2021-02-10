6bb008f7e2796c3602abfaadc5c62c4aecfb8f0a
ajay-8192 <ajaychowdary3699@gmail.com>
2021-02-10 20:08:29 +0530

    Added some tests

--- creditCardValidation.test.js
+++ creditCardValidation.test.js
@@ -5,4 +5,8 @@ test('Should be True or False based on input', () => {
     const text = validationCreditCard('5103720192896460','ajay chowdary', '10/20', '122');
 
     expect(text).toBeFalsy
+
+    const text = validationCreditCard('5103720192896460','ajay chowdary', '10/25', '122');
+
+    expect(text).toBeTruthy
 })
